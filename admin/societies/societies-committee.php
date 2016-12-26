55012b0e162508695a8f28fc5c47627fb7f6d616
Natasha Hadfield <nmhadfield@dundee.ac.uk>
2016-12-26 20:56:36 +0000

    Society Registration

--- admin/societies/societies-committee.php
+++ admin/societies/societies-committee.php
@@ -4,7 +4,7 @@
 		<?php 
 			$members = get_post_meta(get_the_ID(), 'sh_committee', false);
 			for ($i = 0; $i < count($members); $i++) {
-				$user = get_user_by('login', $members[$i]['id']); ?> <tr>
+				$user = get_user_by('ID', $members[$i]['id']); ?> <tr>
 					<td><?php echo($user == null ? $members[$i] :$user -> display_name); ?></td>
 					<td><?php echo($members[$i]['role']); ?></td>
 				</tr> <?php }
